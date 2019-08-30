bc296cb444fd4990de953e39be198c34c07738d5
Gabriel Vitor <gabrielvitor@rede.ulbra.br>
2019-08-30 19:13:35 -0300

    Segundo commit

--- projeto_noticias/app_noticias/models.py
+++ projeto_noticias/app_noticias/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Noticia(models.Model):
+    class Meta:
+        verbose_name = 'Noticia'
+        verbose_name_plural = 'Noticias'
+    titulo = models.CharField('titulo', max_length=128)
+    conteudo = models.TextField()
